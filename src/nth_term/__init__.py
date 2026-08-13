ff6d0b5b5119000af035b738deb10c30d32b1641
devinlin89 <devinlin89@gmail.com>
2026-08-13 17:45:12 +0700

    Update __init__.py for new parsing functionality

--- src/nth_term/__init__.py
+++ src/nth_term/__init__.py
@@ -1,8 +1,10 @@
 from .calculator import find_nth_term
 from .models import NthTermResult, SequenceType
+from .parsing import parse_sequence
 
 __all__ = [
     "NthTermResult",
     "SequenceType",
     "find_nth_term",
+    "parse_sequence"
 ]
